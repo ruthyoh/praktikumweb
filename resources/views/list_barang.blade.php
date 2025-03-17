f7ad5da1556d37b9533ee52f6669776cb10e11e6
ruthyoh <ruthmanurung5112@gmail.com>
2025-03-17 15:04:22 +0700

    terbaru web.php dan penambahan file list_barang

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,6 @@
+<html>
+        <h1>List Barang</h1>
+        <p>Kode Barang: {{$id}}</p>
+        <p>Nama Barang: {{$nama}}</p><!-- Order your soul. Reduce your wants. - Augustine -->
+</div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,33 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-// Route::get('/', function() {
-//     return view('welcome'); 
-// });
+
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/welcome', function() {
+    return view('welcome'); 
+ });
+
+
+ Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+ });
+
+ // routes/web.php 
+
+ Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+    return view('list_barang', compact('id', 'nama'));
+});
