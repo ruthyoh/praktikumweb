f0d1887e01a69d9c2b142ef489ebb6d464014f4c
ruthyoh <ruthmanurung5112@gmail.com>
2025-04-17 14:58:13 +0700

    menambahkan gambar baru

--- resources/views/mobil.blade.php
+++ resources/views/mobil.blade.php
@@ -8,31 +8,47 @@
     
 
 
-
-
-<div class="grid gap-4">
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="images/lamborgini.jpeg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="images/bmw.jpeg" alt="">
     </div>
-    <div class="grid grid-cols-5 gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="images/bmw.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="images/tesla.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="images/hitam.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="images/merah.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="images/xiaomi.jpeg" alt="">
-        </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/hitam.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/lamborghini.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/merah.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/rr.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/tesla.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/xiaomi.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/biru.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/bintang.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/keren.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/putih.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/kuning.jpeg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="images/pink.jpeg" alt="">
     </div>
 </div>
-
 
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-<<<<<<< HEAD
+use App\Http\Controllers\DataController;
 
-=======
-<<<<<<< HEAD
-Route::get('/', function() {
-    return view('welcome'); 
- });
-=======
-// Route::get('/', function() {
-//     return view('welcome'); 
-// });
->>>>>>> 2d7670c2410b2e02a882d743389faa64bcada6e7
->>>>>>> 086e3db7e166e1364e837e46abce44e312c7df18
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
@@ -46,17 +36,6 @@ Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
     return view('list_barang', compact('id', 'nama'));
 });
 
-
-Route::get('/menu', function () {
-    $items = [
-        ['id' => 1, 'name' => 'Beras Pandan Wangi', 'harga' => 15000],
-        ['id' => 2, 'name' => 'Tepung Terigu', 'harga' => 20000],
-        ['id' => 3, 'name' => 'Bagon Cair', 'harga' => 11500],
-        ['id' => 4, 'name' => 'Penyedap Rasa', 'harga' => 12000],
-        ['id' => 5, 'name' => 'Minyak Goreng', 'harga' => 14000],
-    ];
-    return view('menu', compact('items'));
-});
 
 Route::get('/mobil', function () {
     return view('mobil');
